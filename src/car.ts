f26b404a79b6bd52dccf0cfa301b6b65be561c6e
varchodi <rauschenrobert@gmail.com>
2024-02-08 18:45:23 +0300

    control kinda done

--- src/car.ts
+++ src/car.ts
@@ -6,6 +6,7 @@ export default class Car{
     acceleration: number;
     maxSpeed: number;
     friction: number;
+    angle: number;
 
     constructor(public x:number, public y:number,public width:number,public height:number) {
         this.x = x;
@@ -17,6 +18,9 @@ export default class Car{
         this.acceleration = 0.2;
         this.maxSpeed = 3;
         this.friction = .05;
+
+        this.angle =0;
+        //this.angle = Math.PI/2; //maybe better 
         
         this.controls = new Controls();
 
@@ -24,12 +28,27 @@ export default class Car{
 
     //update stuff while car control
     update() {
+        //< n > affect only speed 
         if (this.controls.forward) {
             this.speed += this.acceleration;
         }
         if (this.controls.reverse) {
             this.speed -= this.acceleration;
         }
+
+        //?? this upper if is to handle reverse turning .../.
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            
+                    //this other 2 affects angle
+                    if (this.controls.left) {
+                        this.angle += .03*flip;
+                    }
+                    if (this.controls.right) {
+                        this.angle -= .03*flip;
+                    }
+        }
+            
         //speed stuffs ...
         if (this.speed > this.maxSpeed) {
             this.speed = this.maxSpeed;
@@ -40,22 +59,35 @@ export default class Car{
 
         //fr
         if (this.speed > 0) {
-            this.speed -= this.friction;
+            this.speed -= this.friction; 
         }
         if (this.speed < 0) {
             this.speed += this.friction;
         }
-        this.y -= this.speed;
+
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+        //?? for rotation 
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+        //this.y -= this.speed;
     }
 
     public draw(ctx: CanvasRenderingContext2D) {
+        //?? for rotation
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            - this.width / 2,
+            - this.height / 2,
             this.width,
             this.height
         );
         ctx.fill();
+        ctx.restore();
     }
 }
